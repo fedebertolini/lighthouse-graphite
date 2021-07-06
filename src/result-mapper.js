f8706395b346df49d43f71b13321ce2dde317b11
Robin Nieuwboer <robin.nieuwboer@dpgmm.net>
2021-07-06 09:53:00 +0200

    toFixed instead of floored value

--- src/result-mapper.js
+++ src/result-mapper.js
@@ -1,8 +1,7 @@
-const clsDecimalMultiplier = 10000;
 exports.map = result => ({
     'largest-contentful-paint': Math.floor(result.audits['largest-contentful-paint'].numericValue),
     'total-blocking-time': Math.floor(result.audits['total-blocking-time'].numericValue),
-    'cumulative-layout-shift': Math.floor(result.audits['cumulative-layout-shift'].numericValue * clsDecimalMultiplier) / clsDecimalMultiplier,
+    'cumulative-layout-shift': parseFloat(result.audits['cumulative-layout-shift'].numericValue.toFixed(5)),
     'performance-score': result.categories.performance.score * 100,
     'first-contentful-paint': Math.floor(result.audits['first-contentful-paint'].numericValue),
     'speed-index': Math.floor(result.audits['speed-index'].numericValue),
